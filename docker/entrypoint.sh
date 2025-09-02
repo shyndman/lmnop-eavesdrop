a89980205877b4cf5e12bec9958486952f812b7e
Scott Hyndman <scotty.hyndman@gmail.com>
2025-09-02 04:02:04 -0400

    Remove excessive entry point logging

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-RUN_ID=$(python3 -c "import uuid; print(uuid.uuid4())")
-echo "ENTRYPOINT START - Run ID: $RUN_ID" >&2
-
 source /opt/conda/etc/profile.d/conda.sh
-
-echo "!!! !!! ENVIRONMENT PRE ACTIVATION !!! !!!"
-echo "~~~ Pip ~~~"
-conda run pip list -v
-echo "~~~ Variables ~~~"
-env
 
 conda activate py_3.12
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
-echo "!!! !!! ENVIRONMENT POST ACTIVATION !!! !!!"
-echo "~~~ Pip ~~~"
-conda run pip list -v
+echo "ENVIRONMENT POST ACTIVATION"
 echo "~~~ Variables ~~~"
 env
 
