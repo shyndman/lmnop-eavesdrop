327a763e8cfbf2ed5dc12d3cbdce7a07a4669882
Scott Hyndman <scotty.hyndman@gmail.com>
2026-04-19 08:27:53 -0400

    fix: type FASTER!

--- packages/active-listener/src/active_listener/emitter.py
+++ packages/active-listener/src/active_listener/emitter.py
@@ -83,4 +83,4 @@ class PydotoolTextEmitter:
     if not self._initialized:
       raise RuntimeError("PydotoolTextEmitter.initialize() must run before emit_text()")
 
-    pydotool.type_string(text)
+    pydotool.type_string(text, hold_delay_ms=10, each_char_delay_ms=10)
